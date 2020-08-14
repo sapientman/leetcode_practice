bc9f743f62edc8f791af0bd57283775e69d8ebc1
fuboxiong <1057506789@qq.com>
2020-08-14 09:49:18 +0800

    LRU

---
+++ src/main/java/com/java/algorithm/num/NumMain.java
@@ -0,0 +1,16 @@
+package com.java.algorithm.num;
+
+/**
+ * @author 子荀
+ * @version 1.0
+ * @date 2020/8/13 19:05
+ */
+public class NumMain {
+    public static void main(String[] args) {
+        int a = -5;
+        String s = Integer.toBinaryString(a);
+        String s1 = Integer.toHexString(a);
+        System.out.println(s);
+        System.out.println(s1);
+    }
+}
